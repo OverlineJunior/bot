8cbd391abad43a9829ea9626cd262d7371f556ab
over <franciscosebastianyjunior@hotmail.com>
2025-07-18 14:40:50 -0300

    Send the banned user a DM

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -2,6 +2,7 @@ import { command, parse } from "../command"
 
 const SUCCESS_REPLY = (user: string, reason: string) => `Banned ${user} for: ${reason}`
 const FAILURE_REPLY = "Failed to ban user"
+const DM = (guild: string, reason: string) => `You have been banned from ${guild} for: ${reason}`
 
 export const banCmd = command(
 	'ban',
@@ -13,7 +14,10 @@ export const banCmd = command(
 	(cmd, member, reason) => {
 		member
 			.ban({ reason })
-			.then(() => cmd.reply(SUCCESS_REPLY(member.user.tag, reason)))
+			.then(() => {
+				member.send(DM(cmd.guild!.name, reason))
+				cmd.reply(SUCCESS_REPLY(member.user.tag, reason))
+			})
 			.catch(err => {
 				console.error(`Failed to ban user ´${member.user.tag}´: ${err}`)
 				cmd.reply(FAILURE_REPLY)
