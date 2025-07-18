46fc77062deffef7d4f4322be9a384249aa35076
over <franciscosebastianyjunior@hotmail.com>
2025-07-18 03:22:20 -0300

    Add the ban and the wip unban commands in startCommands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,10 +3,14 @@ import startDmCmd from "./dm"
 import startVerifyCmd from "./verify"
 import startKickCmd from "./kick"
 import startSayCmd from "./say"
+import startBanCmd from "./ban"
+import startUnbanCmd from "./unban"
 
 export default function startCommands(client: Client) {
 	startDmCmd(client)
 	startVerifyCmd(client)
 	startKickCmd(client)
 	startSayCmd(client)
+	startBanCmd(client)
+	startUnbanCmd(client)
 }
