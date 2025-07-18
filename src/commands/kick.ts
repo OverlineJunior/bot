2f1b2e82a41254c9068956a05cab628c7a9647e8
over <franciscosebastianyjunior@hotmail.com>
2025-07-18 14:52:38 -0300

    Add kick DM

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -2,6 +2,7 @@ import { command, parse } from "../command"
 
 const SUCCESS_REPLY = (user: string, reason: string) => `Kicked ${user} for: ${reason}`
 const FAILURE_REPLY = "Failed to kick user"
+const DM = (guild: string, reason: string) => `You have been kicked from ${guild} for: ${reason}`
 
 export const kickCmd = command(
 	'kick',
@@ -13,7 +14,13 @@ export const kickCmd = command(
 	(cmd, member, reason) => {
 		member
 			.kick(reason)
-			.then(() => cmd.reply(SUCCESS_REPLY(member.user.tag, reason)))
+			.then(() => {
+				member
+					.send(DM(cmd.guild!.name, reason))
+					.catch(() => console.log(`Failed to send kick DM to ${member.user.tag}`))
+
+				cmd.reply(SUCCESS_REPLY(member.user.tag, reason))
+			})
 			.catch(err => {
 				console.error(`Failed to kick user ´${member.user.tag}´: ${err}`)
 				cmd.reply(FAILURE_REPLY)
