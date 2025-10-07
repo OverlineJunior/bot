2c72cc7722bbbd8a73f75e1d45c9724d9e798734
over <franciscosebastianyjunior@hotmail.com>
2025-10-07 15:47:47 -0300

    Implement index.ts for commands and list all of them there

---
+++ src/commands/index.ts
@@ -0,0 +1,31 @@
+import { banCmd } from './ban'
+import { dmCmd } from './dm'
+import { forgetCmd } from './forget'
+import { helpCmd } from './help'
+import { kickCmd } from './kick'
+import { remindCmd } from './remind'
+import { remindersCmd } from './reminders'
+import { sayCmd } from './say'
+import { unbanCmd } from './unban'
+import { unwarnCmd } from './unwarn'
+import { verifyCmd } from './verify'
+import { warnCmd } from './warn'
+import { warningsCmd } from './warnings'
+import { xpCmd } from './xp'
+
+export const commands = [
+	dmCmd,
+	verifyCmd,
+	kickCmd,
+	sayCmd,
+	banCmd,
+	helpCmd,
+	unbanCmd,
+	warnCmd,
+	warningsCmd,
+	unwarnCmd,
+	xpCmd,
+	remindCmd,
+	remindersCmd,
+	forgetCmd,
+]
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,9 @@ import 'dotenv/config'
 import { Client } from 'discord.js'
 import startAutoReply from './autoReply'
 import { startCommands } from './command'
-import { banCmd } from './commands/ban'
-import { dmCmd } from './commands/dm'
-import { helpCmd } from './commands/help'
-import { kickCmd } from './commands/kick'
-import { sayCmd } from './commands/say'
-import { verifyCmd } from './commands/verify'
-import { unbanCmd } from './commands/unban'
-import { warnCmd } from './commands/warn'
-import { warningsCmd } from './commands/warnings'
-import { unwarnCmd } from './commands/unwarn'
 import startLeveling from './leveling'
-import { xpCmd } from './commands/xp'
-import { remindCmd } from './commands/remind'
-import { remindersCmd } from './commands/reminders'
-import { forgetCmd } from './commands/forget'
 import { startReminderPoller } from './reminderPoller'
+import { commands } from './commands'
 
 const client = new Client({
 	intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent', 'GuildVoiceStates'],
@@ -27,21 +14,6 @@ startAutoReply(client)
 startLeveling(client)
 startReminderPoller(client)
 
-startCommands(client, [
-	dmCmd,
-	verifyCmd,
-	kickCmd,
-	sayCmd,
-	banCmd,
-	helpCmd,
-	unbanCmd,
-	warnCmd,
-	warningsCmd,
-	unwarnCmd,
-	xpCmd,
-	remindCmd,
-	remindersCmd,
-	forgetCmd,
-])
+startCommands(client, commands)
 
 client.login(process.env.TOKEN)
